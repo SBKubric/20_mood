f2016703e6870fcbbe0482bef58ae72243c9bfe9
SBKubric <sbkubric@gmail.com>
2018-04-17 23:26:37 +0300

    Added quotes and finished page DOM.

--- javascript.js
+++
@@ -1,9 +0,0 @@
-function showRandomQuote() {
-  var quotes_array = JSON.parse(document.getElementById("quotes_json").innerText);
-  var randomNumber = Math.floor(Math.random() * quotes_array.length);
-  document.getElementById("quoteDisplay").innerHTML = quotes_array[randomNumber]['phrase'];
-  if (quotes_array[randomNumber].hasOwnProperty("signature")) {
-    document.getElementById("authorDisplay").innerHTML = quotes_array[randomNumber]['signature'];
-  }
-}
-
---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+function showRandomQuote() {
+  // var switchButton = document.getElementById("switchButton");
+  // if (switchButton.src == "css/switch.png")  {
+  //   switchButton.src = "css/switch_off.png";
+  // }
+  // else {
+  //   switchButton.src = "css/switch.png";
+  // }
+  // var quotes_array = JSON.parse(document.getElementById("quotes_json").innerText);
+  // var randomNumber = Math.floor(Math.random() * quotes_array.length);
+  // document.getElementById("quoteDisplay").innerHTML = quotes_array[randomNumber]['phrase'];
+  // if (quotes_array[randomNumber].hasOwnProperty("signature")) {
+  //   document.getElementById("authorDisplay").innerHTML = quotes_array[randomNumber]['signature'];
+  // }
+}
+
+function turnLightOn() {
+  showRandomQuote()
+}
+
+function turnLightOff() {
+
+}
+
