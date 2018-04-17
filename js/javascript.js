2aec00efec86b712e865926a77be4b8136c89d0d
SBKubric <sbkubric@gmail.com>
2018-04-17 23:31:30 +0300

    Now light can be turned off!

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,38 @@
-function showRandomQuote() {
-  // var switchButton = document.getElementById("switchButton");
-  // if (switchButton.src == "css/switch.png")  {
-  //   switchButton.src = "css/switch_off.png";
-  // }
-  // else {
-  //   switchButton.src = "css/switch.png";
-  // }
-  // var quotes_array = JSON.parse(document.getElementById("quotes_json").innerText);
-  // var randomNumber = Math.floor(Math.random() * quotes_array.length);
-  // document.getElementById("quoteDisplay").innerHTML = quotes_array[randomNumber]['phrase'];
-  // if (quotes_array[randomNumber].hasOwnProperty("signature")) {
-  //   document.getElementById("authorDisplay").innerHTML = quotes_array[randomNumber]['signature'];
-  // }
+function showNext() {
+  let quotes_array = JSON.parse(document.getElementById("quotes_json").innerText);
+  let randomNumber = Math.floor(Math.random() * quotes_array.length);
+  document.getElementById("author").src = ('css/' + quotes_array[randomNumber]["author-image"] + '.png').toString();
+  document.getElementById("speechText").innerHTML = (quotes_array[randomNumber]['phrase'] + ' ' +
+    quotes_array[randomNumber]['signature']).toString();
 }
 
+
 function turnLightOn() {
-  showRandomQuote()
+  let switchButton = document.getElementById("switchButton");
+  switchButton.src = "css/switch.png";
+  let stageBlack = document.getElementById("stage-black");
+  stageBlack.style.opacity = "0";
 }
 
 function turnLightOff() {
-
+  let switchButton = document.getElementById("switchButton");
+  switchButton.src = "css/switch_off.png";
+  let stageBlack = document.getElementById("stage-black");
+  stageBlack.style.opacity = "1";
 }
 
+async function pressSwitch() {
+  let lightIsOn = document.getElementById('switchButton').src.search('switch.png') > -1;
+  if (lightIsOn) {
+    turnLightOff();
+  }
+  else {
+    showNext();
+    await sleep(150);
+    turnLightOn();
+  }
+}
+
+function sleep(ms) {  //  Stolen from stackoverflow
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
